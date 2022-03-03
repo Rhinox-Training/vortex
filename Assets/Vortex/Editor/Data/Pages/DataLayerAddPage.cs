a1c86d5f01358ed6d91e3bbb7deed8048f55d32f
Jorian Baeke <jorian@rhinox.be>
2022-03-03 15:29:58 +0100

    Editor

---
+++ Assets/Vortex/Editor/Data/Pages/DataLayerAddPage.cs
@@ -0,0 +1,57 @@
+using System;
+using Sirenix.OdinInspector;
+using UnityEditor;
+
+namespace Rhinox.Vortex.Editor
+{
+    public class DataLayerAddPage  : DataLayerBaseDataPage
+    {
+        [HideReferenceObjectPicker, NonSerialized, ShowInEditor, VerticalGroup("Yes")]
+        public object Data;
+
+        public DataLayerAddPage(SlidePagedWindowNavigationHelper<object> pager, GenericDataTable dataTable) : base(pager, dataTable)
+        {
+            Data = Activator.CreateInstance(dataTable.DataType);
+        }
+
+        // Store
+        [InfoBox("Primary Key already taken in DataLayer, choose a different key.", InfoMessageType.Error, nameof(PrimaryKeyTaken))]
+        [Button, VerticalGroup("Yes"), DisableIf(nameof(ShouldDisableStore))]
+        private void StoreObject()
+        {
+            if (!StoreObject(Data))
+                return;
+            EditorApplication.delayCall += _pager.NavigateBack;
+        }
+
+        private bool ShouldDisableStore()
+        {
+            return Data == null || PrimaryKeyTaken();
+        }
+
+        private bool PrimaryKeyTaken()
+        {
+            if (Data == null)
+                return false;
+            
+            return ContainsKey(Data);
+        }
+        
+        private bool ContainsKey(object storeObject)
+        {
+            int key = _dataTable.GetID(storeObject); // Fetch ID of new object (not in datalayer)
+            return _dataTable.GetStoredObject(key) != null; // Check if ID is available in table
+        }
+
+        private bool StoreObject(object storeObject)
+        {
+            return _dataTable.StoreObject(storeObject, false);
+        }
+        
+        [Button, VerticalGroup("Yes")]
+        private void Back()
+        {
+            EditorApplication.delayCall += _pager.NavigateBack;
+        }
+    }
+}
---
+++ Assets/Vortex/Editor/Data/Pages/DataLayerEditDataPage.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Reflection;
+using Rhinox.VOLT.Data;
+using Sirenix.OdinInspector;
+using UnityEditor;
+
+namespace Rhinox.Vortex.Editor
+{
+    public class DataLayerEditDataPage : DataLayerBaseDataPage
+    {
+        private const string GROUP_FIX = "DoNotRemove_FixesLayout";
+        private const string BUTTON_GROUP_FIX = GROUP_FIX + "/Buttons";
+        
+        [HideReferenceObjectPicker, HideLabel, NonSerialized, ShowInEditor, VerticalGroup(GROUP_FIX)]
+        public object Data;
+
+        private Action _onEditEntry;
+
+        public DataLayerEditDataPage(SlidePagedWindowNavigationHelper<object> pager, GenericDataTable dataTable, object data,
+            Action onEditEntry = null) 
+            : base(pager, dataTable)
+        {
+            Data = data;
+            _onEditEntry = onEditEntry;
+        }
+        
+        [Button(ButtonSizes.Large), VerticalGroup(GROUP_FIX), HorizontalGroup(BUTTON_GROUP_FIX)]
+        public void Save()
+        {
+            bool storeSucceeded = StoreObject(Data);
+            if (!storeSucceeded)
+                EditorUtility.DisplayDialog("Notice",
+                    "Something went wrong during saving this entry. The entry will remain unchanged!", "Confirm");
+            EditorApplication.delayCall += _pager.NavigateBack;
+            _onEditEntry?.Invoke();
+        }
+
+        [Button(ButtonSizes.Large),VerticalGroup(GROUP_FIX), HorizontalGroup(BUTTON_GROUP_FIX)]
+        public void Cancel()
+        {
+            EditorApplication.delayCall += _pager.NavigateBack;
+        }
+        
+        private bool StoreObject(object storeObject)
+        {
+            return _dataTable.StoreObject(storeObject, true);
+        }
+    }
+}
