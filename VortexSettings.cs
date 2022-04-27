aaa66edccd82e60c98a06234ad58227eeb9c992d
Jorian Baeke <jorian@rhinox.be>
2022-04-27 13:28:31 +0200

    Added dependency on Utilities, changed settings from projectsettings to configfile

--- VortexSettings.cs
+++ VortexSettings.cs
@@ -1,26 +1,15 @@
+using System;
 using System.Reflection;
+using Rhinox.Utilities;
 using Rhinox.Vortex.File;
 using Sirenix.OdinInspector;
 using UnityEngine;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
 namespace Rhinox.Vortex
 {
-    public class VortexSettings : ProjectSettingsProvider<VortexSettings>
+    public class VortexSettings : ConfigFile<VortexSettings>
     {
         [DisableInPlayMode, SerializeReference]
         public EndPointConfiguration Configuration;
-        
-        protected override void Init()
-        {
-            Configuration = new FileEndPointConfig();
-        }
-        
-#if UNITY_EDITOR
-        [SettingsProvider]
-        public static SettingsProvider RegisterProvider() => Instance.CreateSettingsProvider();
-#endif
     }
 }
