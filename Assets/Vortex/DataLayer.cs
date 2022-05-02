9e1a7f89e0c451f3272f5975679154f67a172868
Gaetan Deglorie <gaetan@rhinox.training>
2022-05-02 15:38:10 +0200

    Restored some API functions from old EditorDataLayer + cleanup

--- Assets/Vortex/DataLayer.cs
+++ Assets/Vortex/DataLayer.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Rhinox.GUIUtils.Attributes;
 using Rhinox.Lightspeed;
 using Rhinox.Perceptor;
@@ -16,30 +18,38 @@ namespace Rhinox.Vortex
         private static DataEndPoint _endPoint;
 
         private static Stack<DataEndPoint> _endPointStack;
+        private static Dictionary<DataLayerConfig, DataEndPoint> _endPointOverrides;
+        private static DataEndPoint _defaultEndPoint;
 
         public static DataEndPoint DefaultInit(EndPointConfiguration defaultConfiguration)
         {
+            if (_defaultEndPoint != null)
+            {
+                // TODO: check if defaultConfig settings have changed + should we log a warning?
+                return _defaultEndPoint;
+            }
+            
             if (defaultConfiguration == null)
             {
                 PLog.Warn<VortexLogger>($"DataLayer was not initialized, DataLayerConfig failed to load...");
                 return null;
             }
 
-            var endPoint = defaultConfiguration.CreateEndPoint();
-            _endPointStack = new Stack<DataEndPoint>();
-
-            if (endPoint == null)
+            _defaultEndPoint = defaultConfiguration.CreateEndPoint();
+            if (_defaultEndPoint == null)
             {
                 PLog.Error<VortexLogger>(
                     $"DataLayer failed to initialize, no endpoint was created with the current configuration. Check DataLayerConfig and try again.");
                 return null;
             }
 
-            endPoint.Initialize();
+            _defaultEndPoint.Initialize();
 
             _initialized = true;
-            
-            PushEndPoint(endPoint);
+
+            _endPointOverrides = new Dictionary<DataLayerConfig, DataEndPoint>();
+            _endPointStack = new Stack<DataEndPoint>();
+            PushEndPoint(_defaultEndPoint);
 
             return _endPoint;
         }
@@ -62,7 +72,35 @@ namespace Rhinox.Vortex
             
             return _endPoint.GetTable<T>();
         }
+        
+        public static IReadOnlyDataTable<T> ReadTable<T>()
+        {
+            if (!_initialized) // TODO: do we want this? if not what about editor?
+                DefaultInit(VortexSettings.Instance.Configuration);
+            
+            return _endPoint.ReadTable<T>();
+        }
+        
+        public static void PushEndPointFromConfig(DataLayerConfig config)
+        {
+            if (config == null)
+                throw new ArgumentNullException(nameof(config));
+            
+            if (!_initialized) // TODO: do we want this? if not what about editor?
+                DefaultInit(VortexSettings.Instance.Configuration);
 
+            DataEndPoint endPoint;
+            if (!TryGetEndPoint(config, out endPoint))
+            {
+                PLog.Error<VortexLogger>(
+                    $"Could not find/create EndPoint for config {config.name}, pushing default endpoint...");
+                endPoint = _defaultEndPoint;
+            }
+
+            _endPointStack.Push(endPoint);
+            _endPoint = endPoint;
+        }
+        
         public static void PushEndPoint(DataEndPoint endPoint)
         {
             if (!_initialized) // TODO: do we want this? if not what about editor?
@@ -83,6 +121,60 @@ namespace Rhinox.Vortex
             _endPointStack.Pop();
             _endPoint = _endPointStack.Peek();
             return _endPoint;
+        }
+        
+        /// <summary>
+        /// Returns false if multiple endpoints were found
+        /// </summary>
+        public static bool FindSceneEndPointConfig(out DataLayerConfig endPoint)
+        {
+            endPoint = null;
+            
+            var overrides = UnityEngine.Object.FindObjectsOfType<DataEndPointOverride>();
+            if (overrides.Length > 1)
+            {
+                PLog.Error<VortexLogger>("Cannot determine endpoint for datalayer; Too many overrides...");
+                return false;
+            }
+
+            endPoint = overrides.FirstOrDefault()?.Override;
+            return true;
+        }
+
+        private static bool TryGetEndPoint(DataLayerConfig overrideConfig, out DataEndPoint endPoint)
+        {
+            if (overrideConfig == null)
+            {
+                endPoint = _defaultEndPoint;
+                return false;
+            }
+
+            if (overrideConfig.Config == null)
+            {
+                PLog.Warn<VortexLogger>($"Override config {overrideConfig.name} is not configured properly, using default instead...");
+                endPoint = _defaultEndPoint;
+                return false;
+            }
+
+            if (_endPointOverrides.ContainsKey(overrideConfig))
+            {
+                endPoint = _endPointOverrides[overrideConfig];
+                return true;
+            }
+
+            var overrideEndpoint = overrideConfig.Config.CreateEndPoint();
+            if (overrideEndpoint == null)
+            {
+                PLog.Error<VortexLogger>(
+                    $"DataLayer override {overrideConfig.name} failed to initialize, no endpoint was created with the current configuration. Returning default endpoint.");
+                endPoint = _defaultEndPoint;
+                return false;
+            }
+            
+            overrideEndpoint.Initialize();
+            _endPointOverrides.Add(overrideConfig, overrideEndpoint);
+            endPoint = overrideEndpoint;
+            return true;
         }
     }
 }
